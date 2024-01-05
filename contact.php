679533c9c67007a2d96ae0916719bd813dab90c9
mohamed rabie <mohamrabie9@gmail.com>
2024-01-05 04:19:51 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,11 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    
 
-    // Send email 
+    // Send email to your Gmail address
     mail("mohamrabie9@gmail.com", "Portfolio Contact Form", "Name: $name\nEmail: $email\nMessage: $message");
 
+    // Redirect to the homepage after sending the email
     header("Location: index.html");
     exit();
 }
