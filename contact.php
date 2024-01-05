a9a9f0fd54cf3d6839cfd7fe59dcdf72094ba37f
Moharabie99 <127060071+Moharabie99@users.noreply.github.com>
2024-01-05 04:18:37 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,11 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    
 
-    // Send email 
-    mail("mohamrabie9@gmail.com", "Portfolio Contact Form", "Name: $name\nEmail: $email\nMessage: $message");
+    //
+    mail("mohmrabie9@gmail.com", "Portfolio Contact Form", "Name: $name\nEmail: $email\nMessage: $message");
 
+    //
     header("Location: index.html");
     exit();
 }
